934ee36b5223d0a2a92681f50c838c8e7e1c6bc1
Hobbes <43449480+ignaciuk@users.noreply.github.com>
2019-04-14 16:34:46 -0400

    changes to survey scripts

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friendsArray = [
     {
-        name: "Stripe Digadilly",
-        avatar: "",
+        friendName: "Stripe Digadilly",
+        friendAvatar: "https://images-na.ssl-images-amazon.com/images/I/91A3aNA5dWL._SL1500_.jpg",
         scores: [
             "3", 
             "3", 
@@ -12,6 +12,22 @@ var friendsArray = [
             "3", 
             "3", 
             "3", 
+            "3"
+        ] 
+    }, 
+    {
+        friendName: "Jacob deGrom",
+        friendAvatar: "https://securea.mlb.com/mlb/images/players/head_shot/594798.jpg",
+        scores: [
+            "2", 
+            "2", 
+            "2", 
+            "2", 
+            "2", 
+            "2", 
+            "2", 
+            "2", 
+            "2", 
             "3"
         ] 
     }
